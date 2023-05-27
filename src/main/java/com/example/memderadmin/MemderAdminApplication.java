4f1b267b81d1e117743364b665aa085911cfc6cd
최재용 <choejaeyong@choejaeyong-ui-MacBookPro.local>
2023-05-27 12:28:26 +0900

    Initial commit

---
+++ src/main/java/com/example/memderadmin/MemderAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.memderadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemderAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemderAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/memderadmin/MemderAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memderadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemderAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
