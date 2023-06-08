854fb0e09d968bd42ad0868b0f70ba74c23ea957
jdragon <jdragon@i-screamedu.co.kr>
2023-06-08 17:24:03 +0900

    feat: 내정보보기API-인수테스트 통과(test success)

--- src/main/java/com/example/memderadmin/api/MemberQueryApi.java
+++ src/main/java/com/example/memderadmin/api/MemberQueryApi.java
@@ -1,7 +1,19 @@
 package com.example.memderadmin.api;
 
+import com.example.memderadmin.config.AuthenticationMember;
+import com.example.memderadmin.domain.LoginMember;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberQueryApi {
+
+    @GetMapping("/intsvc/admin/homepage/v1/member/{memberId}")
+    public ResponseEntity<?> getMember(@PathVariable("memberId") Long memberId, @AuthenticationMember LoginMember loginMember) {
+
+        return ResponseEntity.ok().build();
+    }
+
 }
