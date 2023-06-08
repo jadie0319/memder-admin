a621a061e72fcefe9b3f64807f2b4ecf5ebacb01
jdragon <jdragon@i-screamedu.co.kr>
2023-06-08 15:11:27 +0900

    feat: 내정보보기API-인수테스트 생성(test failed)

---
+++ src/main/java/com/example/memderadmin/api/MemberQueryApi.java
@@ -0,0 +1,7 @@
+package com.example.memderadmin.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberQueryApi {
+}
---
+++ src/test/java/com/example/memderadmin/api/MemberQueryApiTest.java
@@ -0,0 +1,56 @@
+package com.example.memderadmin.api;
+
+import com.example.memderadmin.app.LoginRequest;
+import com.example.memderadmin.app.LoginResponse;
+import com.example.memderadmin.app.MemberRegisterRequest;
+import com.example.memderadmin.app.MemberRegisterResponse;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class MemberQueryApiTest extends BaseController {
+
+    @Test
+    void success() {
+        MemberRegisterRequest host = MemberRegisterRequest.host("제이슨", LocalDate.of(2023, 5, 29),
+                "M", "Dudu", "qwer1234!@#", "dudu@gmail.com", "spring");
+        ExtractableResponse<Response> createResponse = MemberRegisterApiTest.callMemberRegisterApi(host);
+        Long memberId = createResponse.body()
+                .as(MemberRegisterResponse.class)
+                .id();
+
+        LoginRequest loginRequest = LoginRequest.of(host.loginId(), host.password());
+        LoginResponse loginResponse = LoginApiTest.callLoginApi(loginRequest)
+                .body()
+                .as(LoginResponse.class);
+
+        ExtractableResponse<Response> response = RestAssured
+                .given()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + loginResponse.token())
+                .pathParam("memberId", memberId)
+                .log().all()
+
+                .when()
+                .get("/intsvc/admin/homepage/v1/member/{memberId}")
+
+                .then()
+                .log().all()
+
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+
+    }
+
+}
